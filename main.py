063863bb81bf399d7fe0a9384a7cce8ae264f06a
QuanHao Guo <49911294+GuoQuanhao@users.noreply.github.com>
2020-12-06 20:37:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ from utils import *
 
 """parsing and configuration"""
 
+# 新增'--start_iter'参数，目的是控制断点续训时断点的步数，用来计算初始学习率
 def parse_args():
-    desc = "Pytorch implementation of U-GAT-IT"
+    desc = "Paddle implementation of U-GAT-IT"
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument('--phase', type=str, default='train', help='[train / test]')
-    parser.add_argument('--light', type=str2bool, default=True, help='[U-GAT-IT full version / U-GAT-IT light version]')
+    parser.add_argument('--light', type=str2bool, default=False, help='[U-GAT-IT full version / U-GAT-IT light version]')
     parser.add_argument('--dataset', type=str, default='selfie2anime', help='dataset_name')
 
     parser.add_argument('--iteration', type=int, default=1000000, help='The number of training iterations')
     parser.add_argument('--batch_size', type=int, default=1, help='The size of batch size')
     parser.add_argument('--print_freq', type=int, default=10000, help='The number of image print freq')
-    parser.add_argument('--save_freq', type=int, default=100000, help='The number of model save freq')
+    parser.add_argument('--save_freq', type=int, default=10000, help='The number of model save freq')
     parser.add_argument('--decay_flag', type=str2bool, default=True, help='The decay_flag')
 
     parser.add_argument('--lr', type=float, default=0.0001, help='The learning rate')
@@ -45,6 +46,7 @@ def check_args(args):
     check_folder(os.path.join(args.result_dir, args.dataset, 'model'))
     check_folder(os.path.join(args.result_dir, args.dataset, 'img'))
     check_folder(os.path.join(args.result_dir, args.dataset, 'test'))
+    check_folder(os.path.join(args.result_dir, args.dataset, 'create'))
 
     # --epoch
     try:
@@ -85,6 +87,10 @@ def main():
 
     if args.phase == 'test' :
         gan.test()
+        print(" [*] Test finished!")
+    
+    if args.phase == 'create' :
+        gan.create()
         print(" [*] Test finished!")
 
 if __name__ == '__main__':
